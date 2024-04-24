f6318d4485b0c997911553cb2c28247ceba7c4b9
Juan Ignacio Toffolon <juanignaciotoffolon@gmail.com>
2024-04-24 18:46:50 -0300

    Probando Cuadros

--- resources/js/Pages/Cuadros/Index.jsx
+++ resources/js/Pages/Cuadros/Index.jsx
@@ -1,26 +1,10 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import React from "react";
+import { CuadrosList } from "@/Components/CuadrosList";
 
-export default function Cuadros({ auth, cuadros }) {
-    console.log(cuadros)
+export default function Cuadros({  cuadros }) {
+    
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Listado de Cuadros</h2>}
-        >
-            <Head title="cuadros" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    {cuadros.map((cuadro) => (
-                            <p className="mt-4 text-sm/relaxed text-white">
-                            {cuadro.name}
-                        </p>
-                    ))}
-                    </div>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+        <CuadrosList cuadros={cuadros} />
+          
+    )
 }
